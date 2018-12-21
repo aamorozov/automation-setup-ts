3a3e310cdaf004a49d63b33caf4886fcafc7cfc9
aamorozov <andreym@hzdg.com>
2018-12-21 16:26:02 -0500

    Adding default webpack config

---
+++ src/static/__tests__/e2e/config/webpack.config.ts
@@ -0,0 +1,13 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: './foo.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'foo.bundle.js'
+  }
+};
+
+export default config;
