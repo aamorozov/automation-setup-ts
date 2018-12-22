55b36bad3f6610c22f5b3debe33e450aa744dec1
aamorozov <andreym@hzdg.com>
2018-12-22 13:22:32 -0500

    Bump package versions

--- __tests__/e2e/nightwatch.conf.ts
+++
@@ -1,6 +0,0 @@
-import { generateConfig } from './config/generateConfig';
-import webpackConfig from './config/webpack.config';
-
-const config = generateConfig(webpackConfig);
-
-module.exports = config;
--- __tests__/e2e/tests/test.ts
+++
@@ -1,6 +0,0 @@
-import {NightwatchBrowser} from 'nightwatch';
-
-export default (client: NightwatchBrowser) => {
-  client.url('https://www.google.com');
-  client.waitAndClick('a[href*="store"]', 5000, false);
-};
