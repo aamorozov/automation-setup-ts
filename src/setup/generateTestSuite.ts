3783803c3978c59d9744a14dfd24b5949fd294ae
aamorozov <andreym@hzdg.com>
2018-12-21 19:19:28 -0500

    Updating test suite generator

--- src/setup/generateTestSuite.ts
+++ src/setup/generateTestSuite.ts
@@ -1,4 +1,5 @@
 import {
+  getStaticDirectories,
   isDirExists,
   promisifiedCopy,
   regection,
@@ -9,21 +10,15 @@ import {
 
 import * as path from 'path';
 
-const staticDirectories = {
-  tests: path.join('__tests__', 'e2e', 'tests'),
-  pageObjects: path.join('__tests__', 'e2e', 'pages'),
-  commands: path.join('__tests__', 'e2e', 'commands'),
-  assertions: path.join('__tests__', 'e2e', 'assertions'),
-  data: path.join('__tests__', 'e2e', 'data'),
-  config: path.join('__tests__', 'e2e', 'config'),
-  utils: path.join('__tests__', 'e2e', 'utils')
-};
+const STATIC_FOLDER_PATH = path.resolve(path.join('src', 'static'));
+const getTestSuitePath = (directory: string) =>
+  path.resolve(path.join('__tests__', 'e2e', directory));
 
 export default async function generateExamples(): Promise<void> {
   const getExistingDirectories = (object: object): string[] | null => {
     const existingObjects = Object.values(object).filter(
       (directory: string): string | null => {
-        if (isDirExists(path.join('__tests__', 'e2e', directory))) {
+        if (isDirExists(getTestSuitePath(directory))) {
           return null;
         } else {
           return directory;
@@ -37,52 +32,59 @@ export default async function generateExamples(): Promise<void> {
     }
   };
 
-  const generateResolvers = (
-    resolveArray: string[]
-  ): Iterable<PromiseLike<any>> => {
-    return resolveArray.map(async directory => {
-      if (isDirExists(directory)) {
-        try {
-          Promise.resolve(warning(`Directory already exists at ${directory}`));
-        } catch (e) {
-          Promise.reject(regection(e));
+  const generateResolvers = async (
+    staticDirectories: string[]
+  ): Promise<void> => {
+    await Promise.all(
+      staticDirectories.map(async directory => {
+        if (isDirExists(directory)) {
+          try {
+            Promise.resolve(
+              warning(`Directory already exists at ${directory}`)
+            );
+          } catch (e) {
+            Promise.reject(regection(e));
+          }
+        } else {
+          try {
+            const from = path.resolve(path.join('src', 'static', directory));
+            const to = path.resolve(path.join('__tests__', 'e2e', directory));
+            try {
+              await promisifiedCopy(from, to);
+            } catch (e) {
+              Promise.reject(e);
+            }
+            Promise.resolve(
+              success(
+                `The directory ${removeWordFromString(
+                  `${directory}`,
+                  './static/'
+                )} was created and copied successfuly!`
+              )
+            );
+          } catch (e) {
+            Promise.reject(
+              regection(
+                `There is an error when coppying the directory: \n ${e}`
+              )
+            );
+          }
         }
-      } else {
-        try {
-          promisifiedCopy(
-            path.join(
-              'src',
-              'static',
-              `${removeWordFromString(directory, './static/')}`
-            ),
-            directory,
-            undefined
-          );
-          Promise.resolve(
-            success(
-              `The directory ${removeWordFromString(
-                `${directory}`,
-                './static/'
-              )} was created and copied successfuly!`
-            )
-          );
-        } catch (e) {
-          Promise.reject(
-            regection(`There is an error when coppying the directory: \n ${e}`)
-          );
-        }
-      }
-    });
+      })
+    );
   };
+
   const runPromises = async () => {
     try {
+      const staticDirectories = await getStaticDirectories(STATIC_FOLDER_PATH);
       const existingDirectories = getExistingDirectories(staticDirectories);
       if (existingDirectories) {
-        await Promise.all(generateResolvers(existingDirectories));
+        await generateResolvers(existingDirectories);
       }
     } catch (e) {
       Promise.reject(regection(e));
     }
   };
-  return runPromises();
+
+  await runPromises();
 }
